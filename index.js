87d61361088a1e2adf7b87e5578462e97b9526bd
majazocom <maja.gedda@zocom.se>
2023-09-07 11:04:19 +0200

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+// Skriv ett nytt skript som spelar "gissa talet" med användaren. Reglerna är:
+// Spelet slumpar ett hemligt tal mellan 1 och 100.
+// Spelet ber spelaren gissa talet. (Skriptet räknar antalet gissningar.)
+// Om spelaren gissar rätt, ska programmet gratulera och visa antalet gissningar.
+// Om spelaren gissar fel, ska programmet tala om ifall det hemliga talet är större eller mindre än det gissade. Sedan upprepar skriptet från steg 2.
+
+// 1. slumpa tal mellan 1-100;
+
+// 2. ge prompt till användaren "Gissa tal mellan 1-100"
